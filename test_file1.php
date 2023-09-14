fa25f0110b34213eae96b28bcad04f68ce006bb9
navi <navjot_2901@yahoo.com>
2023-09-14 20:28:57 +0530

    chnged

--- test_file1.php
+++ test_file1.php
@@ -1,6 +1,2 @@
 <?php
-<<<<<<< HEAD
 echo "This is first file edit local system server changes second attempt. This line added from Navi Lap2. lap2 edit1";
-=======
-echo "This is first file edit local system server changes second attempt. This line added from Navi Lap2";
->>>>>>> 637d3daa9239486f534a00c4ac52223334b10b50
