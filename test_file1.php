725e4ddda1d03287d0b2bebe169d4c79aa235b21
unknown <navjot_2901@yahoo.com>
2023-09-14 19:35:26 +0530

    fix merge

--- test_file1.php
+++ test_file1.php
@@ -1,6 +1,2 @@
 <?php
-<<<<<<< HEAD
-echo "This is first file edit local system server changes second attempt. This line from lap1";
-=======
 echo "This is first file edit local system server changes second attempt. This line added from Navi Lap2";
->>>>>>> 177479da7e0ccd4d5089080c117cb272c4e7636a
